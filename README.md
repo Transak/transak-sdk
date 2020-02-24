4b8130e28cc24509308bf3ee62a732cecaf31376
Yeshu Agarwal <agarwal.yeshu@gmail.com>
2020-02-23 22:34:07 -0800

    Fix building issue in React
    While running npm run build in a react project, it was failing to minify the source code. This issue was happening because of the ES6 syntaxes. Added Babel to fix this issue. Also, updated the readme file.

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# transak-sdk
- Transak SDK that allows you to easily integrate the fiat on/ramp
+# Transak SDK
+[![Build](https://travis-ci.org/joemccann/dillinger.svg)]()
+A JavaScript library for decentralised applications to onboard their global user base with fiat currency.
+### Installation
+To use the Transak widget with your React application, you will need to use `[@transak/transak-sdk](https://www.npmjs.com/package/@transak/transak-sdk)` (Transak’s  JavaScript SDK).
+Add the Transak SDK as a dependency using `yarn` or `npm`:
+```sh
+# Using yarn
+$ yarn add @transak/transak-sdk
+
+# Using npm
+$ npm install @transak/transak-sdk
+```
+### Example usage
+```sh
+import transakSDK from '@transak/transak-sdk'
+
+let transak = new transakSDK({
+    apiKey: '',  // Your API Key
+    environment: 'PRODUCTION', // STAGING/PRODUCTION
+    defaultCryptoCurrency: 'ETH',
+    walletAddress: '', // Your customer's wallet address
+    themeColor: '000000', // App theme color
+    fiatCurrency: '', // INR/GBP
+    email: '', // Your customer's email address
+    redirectURL: '',
+    hostURL: window.location.origin,
+    widgetHeight: '550px',
+    widgetWidth: '450px'
+});
+
+transak.init();
+
+// To get all the events
+transak.on(transak.ALL_EVENTS, (data) => {
+		console.log(data)
+});
+
+// This will trigger when the user marks payment is made.
+transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, (orderData) => {
+    console.log(orderData);
+    transak.close();
+});
+```
+
+For in-depth instructions on integrating Transak, view [our complete documentation.](https://transak.com/integrate)
--- src/index.js
+++ src/index.js
@@ -5,85 +5,79 @@ import {UrlEncode} from "./utils/generalUtil";
 import {closeSVGIcon} from './assets/svg';
 import {getCSS} from './assets/css';
 const eventEmitter = new events.EventEmitter();
-
-class TransakSDK {
-    constructor(partnerData) {
-        this.partnerData = partnerData;
-        this.isInitialised = false;
-        this.EVENTS = EVENTS;
-        this.ALL_EVENTS = '*';
-        this.ERROR = 'TRANSAK_ERROR'
-    }
-
-    on(type, cb) {
-        if (type === this.ALL_EVENTS){
-            for (let eventName in EVENTS){
-                eventEmitter.on(EVENTS[eventName], cb);
-            }
+function TransakSDK(partnerData) {
+    this.partnerData = partnerData;
+    this.isInitialised = false;
+    this.EVENTS = EVENTS;
+    this.ALL_EVENTS = '*';
+    this.ERROR = 'TRANSAK_ERROR'
+}
+TransakSDK.prototype.on = function (type, cb) {
+    if (type === this.ALL_EVENTS) {
+        for (let eventName in EVENTS) {
+            eventEmitter.on(EVENTS[eventName], cb);
         }
-        console.log(type,'type')
-        if (EVENTS[type]) eventEmitter.on(type, cb);
-        if (type === this.ERROR) eventEmitter.on(this.ERROR, cb);
     }
-
-    init() {
-        this.modal(this);
-    }
-
-    close(){
-        let modal = document.getElementById("transakFiatOnOffRamp");
-        modal.style.display = "none";
-        modal.innerHTML = "";
-    }
-
-
-    async closeRequest() {
-        let iframeEl = document.getElementById('transakOnOffRampWidget');
-        if (iframeEl) iframeEl.contentWindow.postMessage({
-            event_id: EVENTS.TRANSAK_WIDGET_CLOSE_REQUEST,
-            data: true
-        }, '*');
-    }
-
-    async modal() {
-        try {
-            if (this.partnerData) {
-                let {url, width, height, partnerData} = await generateURL(this.partnerData);
-                let wrapper = document.createElement('div');
-                wrapper.id = "transakFiatOnOffRamp";
-                wrapper.innerHTML = `<div class="transak_modal-overlay" id="transak_modal-overlay"></div><div class="transak_modal" id="transak_modal"><div class="transak_modal-content"><span class="transak_close">${closeSVGIcon}</span><div class="transakContainer"><iframe id="transakOnOffRampWidget" src="${url}" style="width: ${width}; height: ${height}"></iframe></div></div></div>`;
-                let container = document.getElementsByTagName("body");
-                if (!container) container = document.getElementsByTagName("html");
-                if (!container) container = document.getElementsByTagName("div");
-                await container[0].appendChild(wrapper);
-                await setStyle(this.partnerData.themeColor, width, height);
-                let modal = document.getElementById("transakFiatOnOffRamp");
-                let span = document.getElementsByClassName("transak_close")[0];
-                modal.style.display = "block";
-                this.isInitialised = true;
-                eventEmitter.emit(EVENTS.TRANSAK_WIDGET_INITIALISED, {status: true, eventName : EVENTS.TRANSAK_WIDGET_INITIALISED});
-                // When the user clicks on <span> (x), close the modal
-                span.onclick = () => {
-                    return this.closeRequest()
-                }
-                // When the user clicks anywhere outside of the modal, close it
-                window.onclick = (event) => {
-                    if (event.target === document.getElementById("transak_modal-overlay")) this.closeRequest()
-                }
-                if (window.addEventListener) window.addEventListener("message", handleMessage);
-                else window.attachEvent("onmessage", handleMessage);
+    if (EVENTS[type]) eventEmitter.on(type, cb);
+    if (type === this.ERROR) eventEmitter.on(this.ERROR, cb);
+}
+TransakSDK.prototype.init = function () {
+    this.modal(this);
+}
+TransakSDK.prototype.close = async function () {
+    let modal = document.getElementById("transakFiatOnOffRamp");
+    modal.style.display = "none";
+    modal.innerHTML = "";
+    await modal.remove();
+}
+TransakSDK.prototype.closeRequest = function () {
+    let iframeEl = document.getElementById('transakOnOffRampWidget');
+    if (iframeEl) iframeEl.contentWindow.postMessage({
+        event_id: EVENTS.TRANSAK_WIDGET_CLOSE_REQUEST,
+        data: true
+    }, '*');
+}
+TransakSDK.prototype.modal = async function () {
+    try {
+        if (this.partnerData) {
+            let {url, width, height, partnerData} = await generateURL(this.partnerData);
+            let wrapper = document.createElement('div');
+            wrapper.id = "transakFiatOnOffRamp";
+            wrapper.innerHTML = `<div class="transak_modal-overlay" id="transak_modal-overlay"></div><div class="transak_modal" id="transak_modal"><div class="transak_modal-content"><span class="transak_close">${closeSVGIcon}</span><div class="transakContainer"><iframe id="transakOnOffRampWidget" src="${url}" style="width: ${width}; height: ${height}"></iframe></div></div></div>`;
+            let container = document.getElementsByTagName("body");
+            if (!container) container = document.getElementsByTagName("html");
+            if (!container) container = document.getElementsByTagName("div");
+            await container[0].appendChild(wrapper);
+            await setStyle(this.partnerData.themeColor, width, height);
+            let modal = document.getElementById("transakFiatOnOffRamp");
+            let span = document.getElementsByClassName("transak_close")[0];
+            modal.style.display = "block";
+            this.isInitialised = true;
+            eventEmitter.emit(EVENTS.TRANSAK_WIDGET_INITIALISED, {
+                status: true,
+                eventName: EVENTS.TRANSAK_WIDGET_INITIALISED
+            });
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = () => {
+                return this.closeRequest()
             }
-        } catch (e) {
-            throw(e)
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = (event) => {
+                if (event.target === document.getElementById("transak_modal-overlay")) this.closeRequest()
+            }
+            if (window.addEventListener) window.addEventListener("message", handleMessage);
+            else window.attachEvent("onmessage", handleMessage);
         }
+    } catch (e) {
+        throw(e)
     }
 }
 
-let generateURL = async (configData) => {
+async function generateURL(configData) {
     let partnerData = {}, environment = 'development', queryString = "", width = "100%", height = "100%";
     if (configData) {
         if (configData.apiKey) {
-            if (configData.environment){
+            if (configData.environment) {
                 if (config.ENVIRONMENT[configData.environment]) environment = config.ENVIRONMENT[configData.environment].NAME
             }
             try {
@@ -115,43 +109,57 @@ let generateURL = async (configData) => {
     }
     return {width, height, partnerData, url: `${config.ENVIRONMENT[environment].FRONTEND}?${queryString}`}
 }
-let setStyle = async (themeColor, width, height) => {
+async function setStyle(themeColor, width, height) {
     let style = await document.createElement('style');
     style.innerHTML = getCSS(themeColor, height, width);
     let modal = document.getElementById("transakFiatOnOffRamp");
     if (modal) await modal.appendChild(style);
 }
-
-
 function handleMessage(event) {
     let environment;
     if (event.origin === config.ENVIRONMENT.DEVELOPMENT.FRONTEND) environment = config.ENVIRONMENT.DEVELOPMENT.NAME;
     else if (event.origin === config.ENVIRONMENT.PRODUCTION.FRONTEND) environment = config.ENVIRONMENT.PRODUCTION.NAME;
     else if (event.origin === config.ENVIRONMENT.STAGING.FRONTEND) environment = config.ENVIRONMENT.STAGING.NAME;
     if (environment) {
-        if (event && event.data && event.data.event_id){
+        if (event && event.data && event.data.event_id) {
             switch (event.data.event_id) {
-                case EVENTS.TRANSAK_WIDGET_CLOSE:{
-                    eventEmitter.emit(EVENTS.TRANSAK_WIDGET_CLOSE, {status: true, eventName : EVENTS.TRANSAK_WIDGET_CLOSE});
+                case EVENTS.TRANSAK_WIDGET_CLOSE: {
+                    eventEmitter.emit(EVENTS.TRANSAK_WIDGET_CLOSE, {
+                        status: true,
+                        eventName: EVENTS.TRANSAK_WIDGET_CLOSE
+                    });
                     let modal = document.getElementById("transakFiatOnOffRamp");
                     modal.style.display = "none";
                     modal.innerHTML = "";
+                    modal.remove();
                     break;
                 }
-                case EVENTS.TRANSAK_ORDER_CREATED:{
-                    eventEmitter.emit(EVENTS.TRANSAK_ORDER_CREATED, {status: event.data.data, eventName : EVENTS.TRANSAK_ORDER_CREATED});
+                case EVENTS.TRANSAK_ORDER_CREATED: {
+                    eventEmitter.emit(EVENTS.TRANSAK_ORDER_CREATED, {
+                        status: event.data.data,
+                        eventName: EVENTS.TRANSAK_ORDER_CREATED
+                    });
                     break;
                 }
-                case EVENTS.TRANSAK_ORDER_CANCELLED:{
-                    eventEmitter.emit(EVENTS.TRANSAK_ORDER_CANCELLED, {status: event.data.data, eventName : EVENTS.TRANSAK_ORDER_CANCELLED});
+                case EVENTS.TRANSAK_ORDER_CANCELLED: {
+                    eventEmitter.emit(EVENTS.TRANSAK_ORDER_CANCELLED, {
+                        status: event.data.data,
+                        eventName: EVENTS.TRANSAK_ORDER_CANCELLED
+                    });
                     break;
                 }
-                case EVENTS.TRANSAK_ORDER_SUCCESSFUL:{
-                    eventEmitter.emit(EVENTS.TRANSAK_ORDER_SUCCESSFUL, {status: event.data.data, eventName : EVENTS.TRANSAK_ORDER_SUCCESSFUL});
+                case EVENTS.TRANSAK_ORDER_SUCCESSFUL: {
+                    eventEmitter.emit(EVENTS.TRANSAK_ORDER_SUCCESSFUL, {
+                        status: event.data.data,
+                        eventName: EVENTS.TRANSAK_ORDER_SUCCESSFUL
+                    });
                     break;
                 }
-                case EVENTS.TRANSAK_WIDGET_OPEN:{
-                    eventEmitter.emit(EVENTS.TRANSAK_WIDGET_OPEN, {status: true, eventName : EVENTS.TRANSAK_WIDGET_OPEN});
+                case EVENTS.TRANSAK_WIDGET_OPEN: {
+                    eventEmitter.emit(EVENTS.TRANSAK_WIDGET_OPEN, {
+                        status: true,
+                        eventName: EVENTS.TRANSAK_WIDGET_OPEN
+                    });
                     break;
                 }
                 default : {
@@ -160,7 +168,6 @@ function handleMessage(event) {
         }
     }
 }
-
 export default TransakSDK
 
 
