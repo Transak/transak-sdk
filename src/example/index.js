22730295342fc717fa70c25e8ffb28b69b68a668
Satya Sandeep <satyasandeep786@gmail.com>
2022-11-24 17:27:50 +0530

    fix: Rename bsc to bnb

--- src/example/index.js
+++ src/example/index.js
@@ -18,7 +18,7 @@ let userData = {
 let walletAddressesData = {
     networks : {
         'ethereum' : {address : '0xfF21f4F75ea2BbEf96bC999fEB5Efec98bB3f6F4'},
-        'bsc' : {address : 'bnb1dv5ps9vpj6clar79gkd0jrfmg8c0knrd6m090h', addressAdditionalData : '123456'}
+        'bnb' : {address : 'bnb1dv5ps9vpj6clar79gkd0jrfmg8c0knrd6m090h', addressAdditionalData : '123456'}
     },
     coins : {
         'BTC' : {address : 'bc1qlah8pucrmw8l3evszn8a7ay62gpyg00rzl7p2m'},
