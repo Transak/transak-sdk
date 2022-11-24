add7ee977df5aef658268f039ce599c533b96edb
Satya Sandeep <satyasandeep786@gmail.com>
2022-11-24 17:28:40 +0530

    Fix: Update networks (#22)
    * Fix: Update networks
    
    * fix: Rename bsc to bnb

--- src/example/index.js
+++ src/example/index.js
@@ -17,8 +17,8 @@ let userData = {
 };
 let walletAddressesData = {
     networks : {
-        'erc20' : {address : '0xfF21f4F75ea2BbEf96bC999fEB5Efec98bB3f6F4'},
-        'bep2' : {address : 'bnb1dv5ps9vpj6clar79gkd0jrfmg8c0knrd6m090h', addressAdditionalData : '123456'}
+        'ethereum' : {address : '0xfF21f4F75ea2BbEf96bC999fEB5Efec98bB3f6F4'},
+        'bnb' : {address : 'bnb1dv5ps9vpj6clar79gkd0jrfmg8c0knrd6m090h', addressAdditionalData : '123456'}
     },
     coins : {
         'BTC' : {address : 'bc1qlah8pucrmw8l3evszn8a7ay62gpyg00rzl7p2m'},
