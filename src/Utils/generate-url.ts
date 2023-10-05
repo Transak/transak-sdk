d9fdcf35a7260871aefafe78df2f6bc75e714ff9
prajwal <prajwal@transak.com>
2023-10-05 15:30:36 +0530

    Add query param modifications for the NFT query params

--- src/Utils/generate-url.ts
+++ src/Utils/generate-url.ts
@@ -1,4 +1,5 @@
 import qs from 'query-string';
+import Pako from 'pako';
 import { WebAppUrls } from 'Constants/web-app-urls';
 import { TransakConfig } from 'Types/sdk-config.types';
 import { Environments } from 'Constants/environments';
@@ -12,15 +13,27 @@ export function generateURL(configData: TransakConfig) {
   Object.keys(configData).forEach((key) => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    if (configData[key] instanceof Object) {
+    if (['walletAddressesData', 'userData', 'nftData'].includes(key)) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       partnerData[key] = JSON.stringify(configData[key]);
+      if (key === 'nftData') {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        partnerData[key] = btoa(configData[key]);
+      }
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     } else partnerData[key] = configData[key];
   });
+
+  if (configData && configData.calldata) {
+    const compressCallData = Pako.deflate(configData.calldata);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    partnerData.calldata = compressCallData.toLocaleString();
+  }
 
   queryString = qs.stringify(partnerData, { arrayFormat: 'comma' });
 
