bfaca1a82547a5cc566f1535bdb9be8677c896c7
Maurizio Murru <maurizio.murru93@gmail.com>
2024-10-21 16:53:54 +0200

    Add new conf to get to the orders page.

--- src/Constants/web-app-urls.ts
+++ src/Constants/web-app-urls.ts
@@ -4,4 +4,6 @@ export const WebAppUrls = {
   [Environments.DEVELOPMENT]: 'https://localhost:5005/',
   [Environments.STAGING]: 'https://global-stg.transak.com',
   [Environments.PRODUCTION]: 'https://global.transak.com',
+  [Environments.STAGING_ORDERS]: 'https://global-stg.transak.com/user/orders',
+  [Environments.PRODUCTION_ORDERS]: 'https://global.transak.com/user/orders',
 };
