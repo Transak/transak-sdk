912c02442417789b69b6698149e38baec739f11c
Maurizio Murru <maurizio.murru93@gmail.com>
2024-10-29 16:23:37 +0100

    revert url change

--- src/Constants/web-app-urls.ts
+++ src/Constants/web-app-urls.ts
@@ -6,6 +6,6 @@ export const WebAppUrls = {
   [Environments.PRODUCTION]: 'https://global.transak.com',
   [Environments.STAGING_ORDERS]: 'https://global-stg.transak.com/user/orders',
   [Environments.PRODUCTION_ORDERS]: 'https://global.transak.com/user/orders',
-  [Environments.STAGING_ORDERS_NOT_LOGGED_IN]: 'https://global-stg.transak.com/login/otp',
-  [Environments.PRODUCTION_ORDERS_NOT_LOGGED_IN]: 'https://global.transak.com/login/otp',
+  [Environments.STAGING_ORDERS_NOT_LOGGED_IN]: 'https://global-stg.transak.com/login/email',
+  [Environments.PRODUCTION_ORDERS_NOT_LOGGED_IN]: 'https://global.transak.com/login/email',
 };
